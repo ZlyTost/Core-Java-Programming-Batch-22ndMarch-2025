b45a4053d9321b2810c4446f57f2a0752d643e8d
Shalini Verma <46881467+ShaliniVerma21@users.noreply.github.com>
2025-03-23 12:18:25 +0530

    Add files via upload

---
+++ Day1/demo1.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		//Single Line Comment
+		
+		/*
+		 MultiLine
+		 Comments 
+		 */
+		
+		//Print a message to students
+		//Syso+space
+		
+		//+ is used for string concatenation
+		//  /n escape sequence is used for new line
+		System.out.println("Hello Students ! Daily Excersizes "
+				+ "\nare based on topic wise interview questions. "
+				+ "\nit is must for everyone to finish it on time.");
+	}
+
+}
---
+++ Day1/demo2.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// addition of 2 numbers
+		
+		//create variables
+		int a,b,sum;
+		
+		//initialize values
+		a=5;
+		b=6;
+		
+		//perform calculation
+		sum=a+b;
+		
+		//display result
+		System.out.println("Result is : "+sum);
+
+	}
+
+}
